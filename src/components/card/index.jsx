638d58ca9061cc8bd8c6df900f596fa24dfda5fc
Huu Tuan <hohuutuan1999@gmail.com>
2022-10-29 14:05:33 +0700

    BTVN

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,19 +5,19 @@ import "./index.css";
 Card.propTypes = {};
 
 function Card({ status }) {
-  const[option, setOption]=useState(status);
-  let change=(event)=>{
-    setOption(event.target.value);
+  const[mytask, setMytask]= useState(status);
+  let adjust=(event)=>{
+    setMytask(event.target.value);
   }
   return (
     <div>
         <div className="card-body">
             <h3 className="card-title">title: Task 1</h3>
             <p className="card-text">creator: Author 1 </p>
-            <p id="status">status: {option}</p>
+            <p id="status">status: {mytask}</p>
             <h4>Description: </h4>
             <p>This is a task,This is a task,This is a task,This is a task... </p>
-            <select onChange={change} name="">
+            <select onChange={adjust} name="">
                 <option value="NEW">NEW</option>
                 <option value="DOING">DOING</option>
                 <option value="DONE">DONE</option>
